55d683ca1eaa16886a8b5ab398a94706367c3aa9
3535327@qq.com <3535327@qq.com>
2021-01-05 23:10:50 +0800

    api-aws

--- src/main/java/com/huobi/constant/HuobiOptions.java
+++ src/main/java/com/huobi/constant/HuobiOptions.java
@@ -14,7 +14,8 @@ import com.huobi.constant.enums.ExchangeEnum;
 public class HuobiOptions implements Options {
 
   @Builder.Default
-  private String restHost = "https://api.huobi.pro";
+//  private String restHost = "https://api.huobi.pro";
+  private String restHost = "https://api-aws.huobi.pro";
 
   @Builder.Default
   private String websocketHost = "wss://api.huobi.pro";
